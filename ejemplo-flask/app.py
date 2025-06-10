9b31769b55e75ef486b0755e376a25b9dcd5a460
ikestrella <ikestrella@utpl.edu.ec>
2025-06-10 10:07:34 -0500

    TrabajoClase

--- ejemplo-flask/app.py
+++ ejemplo-flask/app.py
@@ -1,26 +1,29 @@
-import cgi
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    form = cgi.FieldStorage()
-    name = form.getvalue('name', 'Invitado')
+    name = request.args.get('name', 'Invitado')
 
-    # Imprimir el contenido HTML
-    return(f"""
+    return f"""
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejemplo en Flask</title>
+    </head>
     <body>
         <h1>¡Hola, {name}!</h1>
-        <p>Este es un ejemplo básico de un script CGI en Python.</p>
-        <form method="GET" action="hello.py">
+        <p>Este es un ejemplo básico de una aplicación Flask.</p>
+        <form method="GET" action="/">
             <label for="name">Escribe tu nombre: </label>
             <input type="text" id="name" name="name">
             <input type="submit" value="Enviar">
         </form>
     </body>
-    """)
+    </html>
+    """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
